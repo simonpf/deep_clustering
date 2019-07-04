814bdb1331cb823297139de7e8cea56ee208665e
Simon Pfreundschuh <simon.pfreundschuh@chalmers.se>
2019-07-04 20:14:33 +0200

    Working on stacked denoising autoencoder.

--- deep_clustering/models/cae.py
+++ deep_clustering/models/cae.py
@@ -1,11 +1,11 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from deep_clustering.models.modules import Encoder, Decoder
+from deep_clustering.models.modules import Model, Encoder, Decoder
 
-class Cae(nn.Module):
-    def __init__(self, channels, latent_dim, arch = [(32, 2), (32, 2)]):
-        super(Cae, self).__init__()
+class Cae(Model):
+    def __init__(self, name, channels, latent_dim, arch = [(32, 2), (32, 2)]):
+        super(Cae, self).__init__(name)
 
         #
         # Encoder
@@ -38,15 +38,34 @@ class Cae(nn.Module):
             in_channels = c
 
         self.channel_reduction = nn.Conv2d(in_channels, channels, 3, padding = 1)
+        self._train_level = 0
+
+    def _apply_noise(self, x, p = 0.1, noise_type = None):
+        if self._training:
+            r = torch.rand(x.size())
+            mask = r > p
+            return r * x
+        else:
+            return x
+
 
     def encode(self, x):
-        for m in self.encoders:
-            x = m(x)
+        for i, m in enumerate(self.encoders):
+            if i == self._train_level:
+                x = self._apply_noise(x, 0.5)
+                x = m(x)
+                return x
+            else:
+                x = m(x)
         return self.latent_mu(x)
 
     def decode(self, mu):
-        z = self.latent_z(mu)
-        for m in self.decoders:
+        n = len(self.encoders)
+        if self._train_level >= n:
+            z = self.latent_z(mu)
+        else:
+            z = mu
+        for m in self.decoders[max(n - self._train_level - 1, 0):]:
             z = m(z)
         z = self.channel_reduction(z)
         return torch.sigmoid(z)
@@ -54,6 +73,25 @@ class Cae(nn.Module):
     def forward(self, x):
         mu = self.encode(x)
         return (self.decode(mu),)
+
+    def visualize_filter(self, j, n_steps = 100, step_length = 0.1):
+        x = torch.randn((1, 1, 64, 64), requires_grad = True)
+        x = torch.zeros((1, 1, 64, 64), requires_grad = True)
+        
+        for i in range(n_steps):
+            y = self.encode(x)
+            n = y.size()[-1]
+            c = torch.sum(y[0, j, n // 2, n // 2])
+            c.backward()
+            print(c)
+            x.data = x.data + step_length * x.grad
+        return x
+
+        
+
+
+
+
 
 def loss_function(x, x_r):
     return F.binary_cross_entropy(x_r, x, reduction='sum')
--- deep_clustering/models/modules.py
+++ deep_clustering/models/modules.py
@@ -1,6 +1,68 @@
+import os
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from torch.utils.data import DataLoader
+
+class Model(nn.Module):
+    def __init__(self, name, path = None):
+        super(Model, self).__init__()
+
+        self.name = name
+        if path is None:
+            try:
+                self.path.os.environ["MODEL_PATH"]
+            except:
+                self.path = "."
+        else:
+            self.path = path
+
+        output_path = os.path.join(self.path, self.name)
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
+        self.output_path = output_path
+
+
+    def train(self,
+              data_set,
+              optimizer,
+              criterion,
+              n_epochs,
+              dataset_callback):
+
+        cuda = torch.cuda.is_available()
+
+        log_file = os.path.join(self.output_path, "training_log.txt")
+        if os.path.isfile(log_file):
+            log_file = open(log_file, mode = "r+")
+        else:
+            log_file = open(log_file, mode = "w")
+
+
+        for i in range(n_epochs):
+
+            if not dataset_callback is None:
+                dataset_callback(data_set)
+            data_loader = DataLoader(data_set, batch_size = 64)
+
+
+            epoch_loss = 0.0
+
+            for j, x in enumerate(data_loader):
+
+                if cuda:
+                    x.cuda()
+
+                y = self(x)
+                optimizer.zero_grad()
+                loss = criterion(x, *y)
+                loss.backward()
+                optimizer.step()
+
+                epoch_loss += loss.detach().float()
+
+                print("Epoch {0}, batch {1}: {2}".format(i, j, loss.detach().float()))
+
 
 class Encoder(nn.Module):
     def __init__(self, in_channels, out_channels, depth, kernel_width = 3):
